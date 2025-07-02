6de81c2ae40524d8e3fb802b85a9eebb8381e629
Dananjaya75 <dananjayasamarasinghe75@gmail.com>
2025-07-02 09:15:41 +0530

    flowlayoutpanel

--- stuManagement.java
+++ stuManagement.java
@@ -1,7 +1,10 @@
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
+
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class stuManagement {
     public static void main(String[] args) {
@@ -10,8 +13,17 @@ public class stuManagement {
         frame.setSize(400, 300);
         frame.setVisible(true);
 
-        JPanel panel = new JPanel(new GridLayout(1, 2));
+        JPanel panel = new JPanel();
+        FlowLayout lay= new FlowLayout();
+        panel.setLayout(lay);
+
         frame.add(panel);
+
+        JLabel lbname=new JLabel("Name");
+        JTextField txtname=new JTextField(20);
+
+        panel.add(lbname);
+        panel.add(txtname);
 
         // StudentDao.getAll();
     }
