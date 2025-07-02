59834f4c0a9876ea981ff4f97a71a31917f63829
Dananjaya75 <dananjayasamarasinghe75@gmail.com>
2025-07-02 08:58:04 +0530

    frame

--- stuManagement.java
+++ stuManagement.java
@@ -1,7 +1,19 @@
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class stuManagement {
     public static void main(String[] args) {
-        StudentDao.getAll();
+        JFrame frame = new JFrame("Student Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+        frame.add(panel);
+
+        // StudentDao.getAll();
     }
 
 }
